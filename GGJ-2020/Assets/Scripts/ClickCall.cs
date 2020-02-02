e86e5b106933efc89f78905b3c78e02de3542008
Oliver <green-coats@outlook.com>
2020-02-02 15:36:16 +1000

    Added footsteps to player and set up audio play script

--- GGJ-2020/Assets/Scripts/ClickCall.cs
+++ GGJ-2020/Assets/Scripts/ClickCall.cs
@@ -6,6 +6,20 @@ using FMODUnity;
 
 public class ClickCallScript : MonoBehaviour
 {
+    //Create UI usage and management of events??
+    [FMODUnity.EventRef]
+    public string PlayerStateEvent = "";
+    FMOD.Studio.EventInstance playerState;
+
+
+    //One shot sound setup
+    [FMODUnity.EventRef]
+    public string itemClick = "";
+    [FMODUnity.EventRef]
+    public string itemUnclick = "";
+    [FMODUnity.EventRef]
+    public string itemPop = "";
+
     // Start is called before the first frame update
     void Start() {
         
@@ -14,5 +28,20 @@ public class ClickCallScript : MonoBehaviour
     // Update is called once per frame
     void Update() {
         
+    }
+
+    //Make the item click sound
+    public void ClickSound() {
+        FMODUnity.RuntimeManager.PlayOneShot(itemClick, transform.position);
+    }
+
+    //Make the item unclick sound
+    public void UnclickSound() {
+        FMODUnity.RuntimeManager.PlayOneShot(itemUnclick, transform.position);
+    }
+
+    //Make the item pop sound
+    public void PopSound() {
+        FMODUnity.RuntimeManager.PlayOneShot(itemPop, transform.position);
     }
 }
